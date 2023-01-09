dcb559e002d731c7f2e982b91e289357bdfb76fe
Wassim SAMAD <wass08@gmail.com>
2023-01-09 15:43:30 +0900

    restore theme color

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       withNormalizeCSS
       theme={{
         globalStyles: (_theme) => ({
-          colors: {
-            brand: ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82','#AD1374' ],
-          },
-          primaryColor: 'brand',
           body: {
             width: "100vw",
             height: "100vh",
