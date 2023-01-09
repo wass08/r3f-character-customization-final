25b626c5014171b862ac7dfa4931870131d2472c
Wassim SAMAD <wass08@gmail.com>
2023-01-09 15:43:39 +0900

    change theme color

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       withGlobalStyles
       withNormalizeCSS
       theme={{
+        colors: {
+          brand: ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82','#AD1374' ],
+        },
+        primaryColor: 'brand',
         globalStyles: (_theme) => ({
           body: {
             width: "100vw",
